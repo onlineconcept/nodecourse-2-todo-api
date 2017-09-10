19fe5e4580d19f81191414e966a5cc4dc8384ea3
Klaus Pedersen <klaus.pedersen@onlineconcept.dk>
2017-09-10 23:12:17 +0200

    Patch Added to server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} =require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -70,6 +71,37 @@ app.delete('/todos/:id',(req,res)=>{
        res.status(404).send();
    });
 
+});
+
+app.patch('/todos/:id',(req,res)=>{
+   var id = req.params.id;
+
+   console.log(req.body);
+   var body = _.pick(req.body, ['text', 'completed']);
+    if(!ObjectID.isValid(id))
+    {
+        return  res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed)
+    {
+        body.completedAt = new Date().getTime();
+
+    }
+    else
+    {
+        body.completedAt = null;
+        body.completed = false;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body},{new:true}).then((todo)=>{
+        if(!todo)
+        {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(404).send();
+    })
 });
 
 app.listen(port,()=>{
